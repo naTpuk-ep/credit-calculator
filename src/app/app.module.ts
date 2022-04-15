102d1c7ef51c86602575426fc7e22b405dc0fb28
naTpuk-ep <naTpuk.ep@gmail.com>
2022-04-15 19:44:11 +0400

    init: app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputNumberModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
